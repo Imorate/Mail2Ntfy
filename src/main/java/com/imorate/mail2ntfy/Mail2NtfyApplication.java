6dbaa4a4bdae55bed259deb60d773d88e8eab32b
Amir Mohammad Hl <dev.imorate@gmail.com>
2025-12-09 00:25:53 +0330

    Init commit

---
+++ src/main/java/com/imorate/mail2ntfy/Mail2NtfyApplication.java
@@ -0,0 +1,13 @@
+package com.imorate.mail2ntfy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mail2NtfyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mail2NtfyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/imorate/mail2ntfy/Mail2NtfyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imorate.mail2ntfy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mail2NtfyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
